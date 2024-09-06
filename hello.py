28fe8cf05097c23998233143dd8d0a2963b8bede
Vikash Kumar <vikash.kumar05@nagarro.com>
2024-09-06 14:03:58 +0530

    Added api call

--- hello.py
+++ hello.py
@@ -1,3 +1,6 @@
+import os
+import requests
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,3 +9,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route("/metrics")
+def metrics():
+    response = requests.get('http://localhost:{port}/metrics'.format(port=os.environ.get('FLOWER_PORT', 6543)))
+    return response.text
