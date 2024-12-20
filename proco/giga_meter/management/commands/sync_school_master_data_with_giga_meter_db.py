1f25dff74ea093e770bba80377e0419bbf99007d
Vikash Kumar <vikash.kumar05@nagarro.com>
2024-12-20 17:40:18 +0530

    Added provision to run the sync thorugh celery worker

--- proco/giga_meter/management/commands/sync_school_master_data_with_giga_meter_db.py
+++ proco/giga_meter/management/commands/sync_school_master_data_with_giga_meter_db.py
@@ -1,5 +1,6 @@
 import logging
 
+from celery import chain
 from django.core.management.base import BaseCommand
 
 from proco.giga_meter import tasks as giga_meter_tasks
@@ -22,10 +23,16 @@ class Command(BaseCommand):
             help='If provided, it will skip the duplicate task check.'
         )
 
+        parser.add_argument(
+            '--schedule', action='store_true', dest='schedule_tasks', default=False,
+            help='If provided, it will schedule the task on Celery.'
+        )
+
 
     def handle(self, **options):
         iso3_format = options.get('iso3_format')
         force_tasks = options.get('force_tasks')
+        schedule_tasks = options.get('schedule_tasks')
 
         country = None
         country_id = None
@@ -35,13 +42,21 @@ class Command(BaseCommand):
             logger.info('Executing School Master Data Sync utility for country: "{}".'.format(country.name))
             country_id = [country.id,]
 
-        giga_meter_tasks.giga_meter_update_static_data(country_iso3_format=iso3_format, force_tasks=force_tasks)
-        logger.info('Data Sync Completed.')
+        if schedule_tasks:
+            chain(
+                giga_meter_tasks.giga_meter_update_static_data.s(country_iso3_format=iso3_format, force_tasks=force_tasks),
+                giga_meter_tasks.giga_meter_handle_published_school_master_data_row.s(country_id, force_tasks=force_tasks),
+                giga_meter_tasks.giga_meter_handle_deleted_school_master_data_row.s(country_id, force_tasks=force_tasks),
+            ).delay()
 
-        giga_meter_tasks.giga_meter_handle_published_school_master_data_row(country_id, force_tasks=force_tasks)
-        logger.info('Published rows Sync Completed.')
+        else:
+            giga_meter_tasks.giga_meter_update_static_data(country_iso3_format=iso3_format, force_tasks=force_tasks)
+            logger.info('Data Sync Completed.')
 
-        giga_meter_tasks.giga_meter_handle_deleted_school_master_data_row(country_id, force_tasks=force_tasks)
-        logger.info('Deleted rows Sync Completed.')
+            giga_meter_tasks.giga_meter_handle_published_school_master_data_row(country_id, force_tasks=force_tasks)
+            logger.info('Published rows Sync Completed.')
+
+            giga_meter_tasks.giga_meter_handle_deleted_school_master_data_row(country_id, force_tasks=force_tasks)
+            logger.info('Deleted rows Sync Completed.')
 
         logger.info('Completed School Master Data Sync utility for country: "{}" successfully.\n'.format(country.name))
