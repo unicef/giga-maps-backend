d22038ec2b8a654de14d187eb375b8b0c5bb14f4
Vikash Kumar <140630604+vikashkum05@users.noreply.github.com>
2025-01-13 19:50:24 +0530

    Moved Map APIs to Primary DB

--- config/settings/base.py
+++ config/settings/base.py
@@ -491,19 +491,19 @@ COUNTRY_MAP_API_SAMPLING_LIMIT = env('COUNTRY_MAP_API_SAMPLING_LIMIT', default=N
 ADMIN_MAP_API_SAMPLING_LIMIT = env('ADMIN_MAP_API_SAMPLING_LIMIT', default=None)
 
 READ_ONLY_DATABASE_ALLOWED_REQUESTS = [
-    # 'global-stat',
-    # 'get-time-player-data',
-    'tiles-connectivity-view',
+    'global-stat',
+    'get-time-player-data',
+    # 'tiles-connectivity-view',
     'get-latest-week-and-month',
-    # 'list-published-advance-filters',
-    # 'list-published-data-layers',
-    # 'info-data-layer',
-    'map-data-layer',
-    # 'download-schools',
-    # 'download-countries',
+    'list-published-advance-filters',
+    'list-published-data-layers',
+    'info-data-layer',
+    # 'map-data-layer',
+    'download-schools',
+    'download-countries',
     'search-countries-admin-schools',
     # 'get-time-player-data-v2',
-    'tiles-view',
+    # 'tiles-view',
 ]
 
 READ_ONLY_DATABASE_ALLOWED_MODELS = []
