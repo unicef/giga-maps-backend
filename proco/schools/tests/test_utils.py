a90a72292043e64644baf38c4fc860909b5d9e26
Vikash Kumar <vikash.kumar05@nagarro.com>
2025-01-29 19:50:32 +0530

    Fixed broken test cases

--- proco/schools/tests/test_utils.py
+++ proco/schools/tests/test_utils.py
@@ -13,22 +13,22 @@ class UtilsUtilitiesTestCase(TestAPIViewSetMixin, TestCase):
 
     def test_get_coverage_type_utility(self):
         country = CountryFactory()
-        school_one = SchoolFactory(country=country, location__country=country)
+        school_one = SchoolFactory(country=country)
         self.assertEqual(type(schools_utilities.get_coverage_type(school_one)), str)
 
     def test_get_connectivity_status_utility(self):
         country = CountryFactory()
-        school_one = SchoolFactory(country=country, location__country=country)
+        school_one = SchoolFactory(country=country)
         self.assertEqual(type(schools_utilities.get_connectivity_status(school_one)), str)
 
     def test_get_connectivity_status_by_master_api_utility(self):
         country = CountryFactory()
-        school_one = SchoolFactory(country=country, location__country=country)
+        school_one = SchoolFactory(country=country)
         self.assertEqual(type(schools_utilities.get_connectivity_status_by_master_api(school_one)), str)
 
     def test_get_get_coverage_status_utility(self):
         country = CountryFactory()
-        school_one = SchoolFactory(country=country, location__country=country)
+        school_one = SchoolFactory(country=country)
         self.assertEqual(type(schools_utilities.get_coverage_status(school_one)), str)
 
     def test_update_school_from_country_or_school_weekly_update_utility(self):
