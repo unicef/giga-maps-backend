58673f19ff28fdea47ea14fcdc5d929a68eb013c
Gaurav Gupta <GGupta5@schools.nyc.gov>
2025-08-28 18:52:03 +0530

    feat: info api - add list of school_ids for schools sharing same lat-long

--- proco/accounts/api.py
+++ proco/accounts/api.py
@@ -1778,6 +1778,31 @@ class DataLayerInfoViewSet(BaseDataLayerAPIViewSet):
 
         return query.format(**kwargs)
 
+    def get_school_ids_at_same_location(self, school_id, country_id):
+        """Get school_ids at same location"""
+        query = """
+        SELECT
+            s.id
+        FROM
+            schools_school s
+        LEFT JOIN
+            connection_statistics_schoolrealtimeregistration srr
+            ON s.id = srr.school_id AND srr.deleted IS NULL
+        WHERE
+            s.deleted IS NULL
+            AND s.id != {school_id}
+            AND s.country_id = {country_id}
+            AND s.geopoint = (SELECT geopoint FROM schools_school WHERE id = {school_id} AND deleted IS NULL)
+        ORDER BY
+            COALESCE(srr.rt_registered, false) DESC
+        """.format(school_id=school_id, country_id=country_id)
+        response = []
+        sql_response = db_utilities.sql_to_response(query, label=self.__class__.__name__, db_var=settings.READ_ONLY_DB_KEY)
+        if sql_response:
+            response = [r.get('id') for r in sql_response]
+            return response
+        return response
+
     def get(self, request, *args, **kwargs):
         use_cached_data = self.request.query_params.get(self.CACHE_KEY, 'on').lower() in ['on', 'true']
         request_path = remove_query_param(request.get_full_path(), 'cache')
@@ -1905,6 +1930,10 @@ class DataLayerInfoViewSet(BaseDataLayerAPIViewSet):
                                 'convert_unit': self.kwargs.get('convert_unit'),
                                 'display_unit': display_unit,
                             }
+                            if request.query_params.get('include_same_location_schools') == 'true':
+                                info_panel_school['same_location_school_ids'] = self.get_school_ids_at_same_location(
+                                    info_panel_school.get('id'), info_panel_school.get('country_id')
+                                )
 
                     response = info_panel_school_list
                 else:
@@ -2006,6 +2035,10 @@ class DataLayerInfoViewSet(BaseDataLayerAPIViewSet):
                             info_panel_school['geopoint'] = json.loads(info_panel_school['geopoint'])
                             info_panel_school['statistics'] = list(filter(
                                 lambda s: s['school_id'] == info_panel_school['id'], statistics))[-1]
+                            if request.query_params.get('include_same_location_schools') == 'true':
+                                info_panel_school['same_location_school_ids'] = self.get_school_ids_at_same_location(
+                                    info_panel_school.get('id'), info_panel_school.get('country_id')
+                                )
 
                     response = info_panel_school_list
                 else:
