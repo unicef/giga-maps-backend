718cc6bcfe4fac7b990275362a34283f15c1ddc5
Vikash Kumar <vikash.kumar05@nagarro.com>
2025-10-31 11:18:34 +0000

    Started adding new model fields

--- proco/data_sources/models.py
+++ proco/data_sources/models.py
@@ -10,9 +10,10 @@ from simple_history.models import HistoricalRecords
 from proco.core import models as core_models
 from proco.locations.models import Country
 from proco.schools import models as school_models
+from proco.entities import models as entity_models
 
 
-class SchoolMasterData(TimeStampedModel, core_models.DataSourceModelMixin):
+class SchoolMasterData(TimeStampedModel, core_models.MasterDataSourceModelMixin):
     """
     SchoolMasterData
         This class define model used to store School Master Data.
@@ -42,12 +43,7 @@ class SchoolMasterData(TimeStampedModel, core_models.DataSourceModelMixin):
     school_id_giga = models.CharField(max_length=50, null=False, blank=False, db_index=True)  # School.giga_id_school
     school_id_govt = models.CharField(blank=True, null=True, max_length=255, db_index=True)  # School.external_id
     school_name = models.CharField(max_length=1000, default='Name unknown')  # School.name
-    admin1 = models.CharField(max_length=255, blank=True, null=True)
-    admin1_id_giga = models.CharField(max_length=50, null=True, blank=True)  # School.admin1
-    admin2 = models.CharField(max_length=255, blank=True, null=True)
-    admin2_id_giga = models.CharField(max_length=50, null=True, blank=True)  # School.admin2
-    latitude = models.FloatField(blank=True, default=None, null=True)  # School.geopoint
-    longitude = models.FloatField(blank=True, default=None, null=True)  # School.geopoint
+
     education_level = models.CharField(blank=True, null=True, max_length=255)  # School.education_level
     education_level_govt = models.CharField(blank=True, null=True, max_length=255)  # School.education_level_govt
     school_area_type = models.CharField(blank=True, null=True, max_length=255)  # School.environment
@@ -82,10 +78,6 @@ class SchoolMasterData(TimeStampedModel, core_models.DataSourceModelMixin):
     nearest_UMTS_distance = models.FloatField(blank=True, default=None, null=True)  # nearest_umts_distance
     nearest_GSM_distance = models.FloatField(blank=True, default=None, null=True)  # nearest_gsm_distance
     nearest_NR_distance = models.FloatField(blank=True, default=None, null=True)  # nearest_nr_distance
-
-    pop_within_1km = models.PositiveIntegerField(blank=True, default=None, null=True)  # pop_within_1km
-    pop_within_2km = models.PositiveIntegerField(blank=True, default=None, null=True)  # pop_within_2km
-    pop_within_3km = models.PositiveIntegerField(blank=True, default=None, null=True)  # pop_within_3km
 
     school_data_source = models.CharField(blank=True, null=True, max_length=255)  # school_data_source
     # school_data_collection_year
@@ -126,80 +118,147 @@ class SchoolMasterData(TimeStampedModel, core_models.DataSourceModelMixin):
     building_id_govt = models.CharField(blank=True, null=True, max_length=255)  # building_id_govt
     num_schools_per_building = models.PositiveIntegerField(blank=True, default=None, null=True)  # num_schools_per_building
 
-    # No Mapping
-    connectivity = models.CharField(blank=True, null=True, max_length=255)
 
-    version = models.PositiveIntegerField(blank=True, default=None, null=True)
 
-    # When pulled from Source API
-    ROW_STATUS_DRAFT = 'DRAFT'
-    # When updated by Editor after pull
-    ROW_STATUS_UPDATED_IN_DRAFT = 'UPDATED_IN_DRAFT'
-    # Send for the publishing by Editor to Publisher
-    ROW_STATUS_DRAFT_LOCKED = 'DRAFT_LOCKED'
-    # Updated by the Publisher
-    ROW_STATUS_UPDATED_IN_DRAFT_LOCKED = 'UPDATED_IN_DRAFT_LOCKED'
-    # Published by Publisher
-    ROW_STATUS_PUBLISHED = 'PUBLISHED'
-    # Deleted by Publisher
-    ROW_STATUS_DELETED = 'DELETED'
-    ROW_STATUS_DELETED_PUBLISHED = 'DELETED_PUBLISHED'
-    ROW_STATUS_DISCARDED = 'DISCARDED'
+class HealthEntityMasterData(TimeStampedModel, core_models.MasterDataSourceModelMixin):
+    """
+    HealthEntityMasterData
+        This class define model used to store Health Entity Master Data.
+    Inherits : `DataSourceModelMixin`
+    """
 
-    ROW_STATUS_CHOICES = (
-        (ROW_STATUS_DRAFT, 'In Draft'),
-        (ROW_STATUS_UPDATED_IN_DRAFT, 'UPDATED BY EDITOR'),
-        (ROW_STATUS_DRAFT_LOCKED, 'ASSIGNED To PUBLISHER'),
-        (ROW_STATUS_UPDATED_IN_DRAFT_LOCKED, 'UPDATED BY PUBLISHER'),
-        (ROW_STATUS_PUBLISHED, 'Published'),
-        (ROW_STATUS_DELETED, 'Deleted'),
-        (ROW_STATUS_DELETED_PUBLISHED, 'Published Deleted'),
-        (ROW_STATUS_DISCARDED, 'Discarded'),
-    )
+    DATA_VERSION_CACHE_KEY = 'health_entity_master_data_last_version_{0}'
 
-    status = models.CharField(max_length=50, choices=ROW_STATUS_CHOICES, default=ROW_STATUS_DRAFT, db_index=True)
-
-    modified_by = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
+    entity = models.ForeignKey(
+        entity_models.Entity,
         blank=True,
         null=True,
-        related_name='updated_%(class)ss',
+        related_name='health_entity',
         on_delete=models.DO_NOTHING,
-        verbose_name='Last Updated By'
+        verbose_name='Health Entity'
     )
 
-    published_by = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
+    country = models.ForeignKey(
+        Country,
         blank=True,
         null=True,
-        related_name='source_published_%(class)ss',
+        related_name='health_entity_master_rows',
         on_delete=models.DO_NOTHING,
-        verbose_name='Published By'
     )
-    published_at = core_models.CustomDateTimeField(db_index=True, null=True, blank=True)
 
-    is_read = models.BooleanField(default=False)
+    # Health Entity Specific Fields
+    health_id_giga = models.CharField(max_length=50, null=False, blank=False, db_index=True)  # School.giga_id_school
+    dhis2_id = models.CharField(blank=True, null=True, max_length=255)  # School.external_id
+    hims_id = models.CharField(blank=True, null=True, max_length=255)  # School.external_id
+    hfml_id = models.CharField(blank=True, null=True, max_length=255)  # School.external_id
+    facility_name = models.CharField(max_length=1000, default='Name unknown')  # School.name
+    facility_type = models.CharField(blank=True, null=True, max_length=255)  # School.external_id
+    facility_ownership = models.CharField(blank=True, null=True, max_length=255)  # School.external_id
 
-    history = HistoricalRecords(inherit=True)
+    admin_3 = models.CharField(blank=True, null=True, max_length=255)
+    admin_4 = models.CharField(blank=True, null=True, max_length=255)
 
-    class Meta:
-        ordering = ['created']
+    connectivity_type = models.CharField(blank=True, null=True, max_length=255)
 
-    @classmethod
-    def get_last_version(cls, iso3_format):
-        last_data_version = cache.get(cls.DATA_VERSION_CACHE_KEY.format(iso3_format))
-        if not last_data_version:
-            latest_records = cls.objects.filter(
-                country__iso3_format=iso3_format,
-                version__isnull=False,
-            ).order_by('-created').first()
-            if latest_records:
-                last_data_version = latest_records.version
-        return last_data_version
+    num_community_health_workers = models.PositiveIntegerField(blank=True, default=None, null=True)
+    num_community_health_workers_within_5km = models.PositiveIntegerField(blank=True, default=None, null=True)
+    area_type = models.CharField(blank=True, null=True, max_length=255)
 
-    @classmethod
-    def set_last_version(cls, value, iso3_format):
-        cache.set(cls.DATA_VERSION_CACHE_KEY.format(iso3_format), value)
+    pop_within_5km = models.PositiveIntegerField(blank=True, default=None, null=True)
+    pop_within_10km = models.PositiveIntegerField(blank=True, default=None, null=True)
+
+    govt_pop_est = models.PositiveIntegerField(blank=True, default=None, null=True)
+    hf_pop_est = models.PositiveIntegerField(blank=True, default=None, null=True)
+    is_facility_open = models.CharField(blank=True, null=True, max_length=10) # Yes/No/Null
+    health_service_provider = models.CharField(blank=True, null=True, max_length=255) # Public|Private|Charitable|Other
+    facility_accessibility = models.CharField(blank=True, null=True, max_length=10) # Yes/No/Null/Unknown
+    # distance_to_closest_settlement
+    # distance_to_country_boundary
+    # facility_level
+    # num_healthworkers
+
+    schools_within_1km = models.PositiveIntegerField(blank=True, default=None, null=True)  # schools_within_1km
+    schools_within_2km = models.PositiveIntegerField(blank=True, default=None, null=True)  # schools_within_2km
+    schools_within_3km = models.PositiveIntegerField(blank=True, default=None, null=True)  # schools_within_3km
+
+
+    facility_id_govt = models.CharField(blank=True, null=True, max_length=255, db_index=True)  # School.external_id
+
+
+
+    education_level = models.CharField(blank=True, null=True, max_length=255)  # School.education_level
+    education_level_govt = models.CharField(blank=True, null=True, max_length=255)  # School.education_level_govt
+
+    school_funding_type = models.CharField(blank=True, null=True, max_length=255)  # School.school_type
+    school_establishment_year = models.PositiveSmallIntegerField(blank=True, default=None, null=True)
+
+    # SchoolWeeklyStatus Fields
+    download_speed_contracted = models.FloatField(blank=True, default=None, null=True)  # download_speed_contracted
+    num_computers_desired = models.PositiveIntegerField(blank=True, default=None, null=True)  # num_computers_desired
+    electricity_type = models.CharField(blank=True, null=True, max_length=255)  # electricity_type
+    num_adm_personnel = models.PositiveIntegerField(blank=True, default=None, null=True)  # num_adm_personnel
+    num_students = models.PositiveIntegerField(blank=True, default=None, null=True)  # num_students
+    num_teachers = models.PositiveIntegerField(blank=True, default=None, null=True)  # num_teachers
+    num_classrooms = models.PositiveIntegerField(blank=True, default=None, null=True)  # num_classroom
+    num_latrines = models.PositiveIntegerField(blank=True, default=None, null=True)  # num_latrines
+    water_availability = models.CharField(blank=True, null=True, max_length=255)  # running_water
+    electricity_availability = models.CharField(blank=True, null=True, max_length=255)  # electricity_availability
+    computer_lab = models.CharField(blank=True, null=True, max_length=255)  # computer_lab
+    num_computers = models.PositiveIntegerField(blank=True, default=None, null=True)  # num_computers
+    connectivity_govt = models.CharField(blank=True, null=True, max_length=255)  # connectivity
+    connectivity_type_govt = models.CharField(blank=True, null=True, max_length=255)  # connectivity_type
+    cellular_coverage_availability = models.CharField(blank=True, null=True, max_length=255)  # coverage_availability
+    cellular_coverage_type = models.CharField(blank=True, null=True, max_length=255)  # coverage_type
+    fiber_node_distance = models.FloatField(blank=True, default=None, null=True)  # fiber_node_distance
+    microwave_node_distance = models.FloatField(blank=True, default=None, null=True)  # microwave_node_distance
+
+
+
+    nearest_LTE_distance = models.FloatField(blank=True, default=None, null=True)  # nearest_lte_distance
+    nearest_UMTS_distance = models.FloatField(blank=True, default=None, null=True)  # nearest_umts_distance
+    nearest_GSM_distance = models.FloatField(blank=True, default=None, null=True)  # nearest_gsm_distance
+    nearest_NR_distance = models.FloatField(blank=True, default=None, null=True)  # nearest_nr_distance
+
+
+
+    school_data_source = models.CharField(blank=True, null=True, max_length=255)  # school_data_source
+    # school_data_collection_year
+    school_data_collection_year = models.PositiveSmallIntegerField(blank=True, default=None, null=True)
+    # school_data_collection_modality
+    school_data_collection_modality = models.CharField(blank=True, null=True, max_length=255)
+    # school_location_ingestion_timestamp
+    school_location_ingestion_timestamp = core_models.CustomDateTimeField(null=True, blank=True)
+    # connectivity_govt_ingestion_timestamp
+    connectivity_govt_ingestion_timestamp = core_models.CustomDateTimeField(null=True, blank=True)
+    # connectivity_govt_collection_year
+    connectivity_govt_collection_year = models.PositiveSmallIntegerField(blank=True, default=None, null=True)
+    disputed_region = models.CharField(blank=True, null=True, max_length=255)  # disputed_region
+
+    # SchoolRealTimeRegistration
+    connectivity_RT = models.CharField(blank=True, null=True, max_length=255)  # rt_registered
+    connectivity_RT_datasource = models.CharField(blank=True, null=True, max_length=255)  # rt_source
+    connectivity_RT_ingestion_timestamp = core_models.CustomDateTimeField(null=True, blank=True)  # rt_registration_date
+
+    download_speed_benchmark = models.FloatField(blank=True, default=None, null=True)  # download_speed_benchmark
+
+    # New fields on 23rd Sept
+    computer_availability = models.CharField(blank=True, null=True, max_length=255)  # computer_availability
+
+    num_students_girls = models.PositiveIntegerField(blank=True, default=None, null=True)  # num_students_girls
+    num_students_boys = models.PositiveIntegerField(blank=True, default=None, null=True)  # num_students_boys
+    num_students_other = models.PositiveIntegerField(blank=True, default=None, null=True)  # num_students_other
+    num_teachers_female = models.PositiveIntegerField(blank=True, default=None, null=True)  # num_teachers_female
+    num_teachers_male = models.PositiveIntegerField(blank=True, default=None, null=True)  # num_teachers_male
+
+    teachers_trained  = models.CharField(blank=True, null=True, max_length=255)  # teachers_trained
+    sustainable_business_model = models.CharField(blank=True, null=True, max_length=255)  # sustainable_business_model
+    device_availability = models.CharField(blank=True, null=True, max_length=255)  # device_availability
+
+    num_tablets = models.PositiveIntegerField(blank=True, default=None, null=True)  # num_tablets
+    num_robotic_equipment = models.PositiveIntegerField(blank=True, default=None, null=True)  # num_robotic_equipment
+
+    building_id_govt = models.CharField(blank=True, null=True, max_length=255)  # building_id_govt
+    num_schools_per_building = models.PositiveIntegerField(blank=True, default=None, null=True)  # num_schools_per_building
 
 
 class DailyCheckAppMeasurementData(core_models.DataSourceModelMixin):
