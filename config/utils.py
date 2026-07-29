e5f799eccb0905b639453ca897eaa0fc8c04e9cb
Maruh Phronesis Vulcan Akporowho <59612588+Maruhsoft@users.noreply.github.com>
2026-07-29 02:44:37 +0000

    Make metrics endpoint resilient and guard get_git_hash against missing git

--- config/utils.py
+++ config/utils.py
@@ -1,7 +1,8 @@
-import git
-
-
 def get_git_hash():
-    repo = git.Repo(search_parent_directories=True)
-    sha = repo.head.commit.hexsha
-    return repo.git.rev_parse(sha, short=4)
+    try:
+        import git
+        repo = git.Repo(search_parent_directories=True)
+        sha = repo.head.commit.hexsha
+        return repo.git.rev_parse(sha, short=4)
+    except Exception:
+        return 'unknown'
